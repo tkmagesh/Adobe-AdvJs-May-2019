786bf75596859457222de41ef1fff72997e85856
Magesh <tkmagesh77@gmail.com>
2019-05-08 15:35:57 +0530

    worker progress implementation

--- 08-web-workers/worker-task.js
+++ 08-web-workers/worker-task.js
@@ -1,14 +1,19 @@
 function doWork(){
-	for(var i=0; i < 20000; i++)
+	for(var i=0; i < 20000; i++){
+		if (i % 100 === 0){
+			var percentCompleted = Math.round((i / 20000) * 100);
+			self.postMessage({ type : 'progress', percentCompleted : percentCompleted});
+		}
 		for(var j=0; j < 10000; j++)
 			for(var k=0; k<100; k++){
 				
 			}
+	}
 }
 
 self.addEventListener('message', function(evtArg){
-	if (evtArg.data === 'start'){
+	if (evtArg.data.type === 'start'){
 		doWork();
-		self.postMessage('completed');
+		self.postMessage({ type : 'completed'});
 	}
 })
